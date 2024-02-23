fe8a3f239f6b520e29d88d4f2c82bfb9267b2de4
mahogany <cjcalimlim7@gmail.com>
2024-02-23 17:31:22 +0800

    Functions that applies discount in the array

---
+++ js-activity/discounted-books.js
@@ -0,0 +1,9 @@
+import { books } from "./books_data";
+
+const applyDiscountPriceOfBooks = (books, value) => {
+    books.map(book => { 
+    book.price = book.price - (Math.round(book.price * value/100).toFixed(2)); 
+    return book;
+  });
+    return books;
+};
