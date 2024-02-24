1758ba5916ece45b829e2821af6573cf97b203e9
Jepjep <jeffreyechano@gmail.com>
2024-02-24 17:03:26 +0800

    final commit for solo genres

--- js-activity/solo_genres.js
+++ js-activity/solo_genres.js
@@ -1,5 +1,5 @@
 export function getSoleRepresentative(books){
-    const genres = books.map(book => books.genre);
+    const genres = books.map(book => book.genre);
     const soloGenres = genres.filter(function(genre, index, bookGenres) {
         return bookGenres.indexOf(genre) === index && bookGenres.lastIndexOf(genre) === index;
     })
