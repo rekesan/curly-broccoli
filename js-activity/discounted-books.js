d74668999cb36c324257e13c9a2c28a96ea56e30
mahogany <cjcalimlim7@gmail.com>
2024-02-25 21:55:31 +0800

    fixed decimal value output

--- js-activity/discounted-books.js
+++ js-activity/discounted-books.js
@@ -1,6 +1,6 @@
 export const applyDiscountPriceOfBooks = (books, value) => {
     books.map(book => { 
-    book.price = book.price - (Math.round(book.price * value/100).toFixed(2)); 
+    book.price = ((book.price) * (1 - (value/100))).toFixed(2); 
     return book;
   });
     return books;
