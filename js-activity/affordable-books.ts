19cd388523aa6bf9b40152a8fa4b2efcbb7eff6d
LeannotFound <113871679+LeannotFound@users.noreply.github.com>
2024-03-02 11:15:24 +0800

    added type annotations

--- js-activity/affordable-books.ts
+++ js-activity/affordable-books.ts
@@ -1,5 +1,7 @@
 // get all titles of books that is below $10 dollars.
-function getAffordableBooks(books) {
+import { BooksArray } from "./books_data";
+
+function getAffordableBooks(books : BooksArray[]):string[]{
   return books.filter((book) => book.price < 10).map((book) => book.title);
 }
 export default getAffordableBooks;
--- js-activity/books_data.ts
+++ js-activity/books_data.ts
@@ -1,4 +1,11 @@
-export const books = [
+export interface BooksArray{
+  title : string;
+  author : string;
+  year : number;
+  genre : string;
+  price : number;
+}
+export const books : BooksArray[] = [
   { title: "The Hobbit", author: "J.R.R. Tolkien", year: 1937, genre: "Fantasy", price: 10.99 },
   { title: "Harry Potter and the Philosopher's Stone", author: "J.K. Rowling", year: 1997, genre: "Fantasy", price: 9.99 },
   { title: "1984", author: "George Orwell", year: 1949, genre: "Dystopian", price: 8.99 },
@@ -9,3 +16,4 @@ export const books = [
   { title: "The Diary of a Young Girl", author: "Anne Frank", year: 1947, genre: "Non-fiction", price: 10.99 },
   { title: "The Catcher in the Rye", author: "J.D. Salinger", year: 1951, genre: "Fiction", price: 9.49 }
 ];
+
